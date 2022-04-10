612a0700d5a79d3c3f54ee6a829a1ef2c061c899
Davina Leong <davina.leong.sy@gmail.com>
2022-04-10 21:53:29 +0800

    Able to toggle menu

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,21 +1,19 @@
-window.onload(() => {
-    const toggle = document.querySelector('button[data-toggle="menu"] img')
-    const mainHeader = document.querySelector(".main-header")
-    const activeClass = 'active';
+const toggle = document.querySelector('.btn-collapse')
+const mainNav = document.querySelector(".main-nav")
+const activeClass = 'active';
 
-    toggle.addEventListener('click', () => {
-        console.log('Toggle clicked')
-        mainHeader.classList.toggle(activeClass)
-    })
-
-    window.onresize(() => {
-        if (window.innerWidth >= 768) {
-            mainHeader.classList.remove(activeClass)
-        }
-    })
-
-    const body = document.querySelector("body")
-    const mainFooter = document.querySelector(".main-footer")
-    body.style.paddingTop = mainHeader.clientHeight
-    body.style.paddingBottom = mainFooter.clientHeight
+toggle.addEventListener('click', () => {
+    mainNav.classList.toggle(activeClass)
 })
+
+window.onresize(() => {
+    if (window.innerWidth >= 768) {
+        mainNav.classList.remove(activeClass)
+    }
+})
+
+const body = document.querySelector("body")
+const mainHeader = document.querySelector(".main-header")
+const mainFooter = document.querySelector(".main-footer")
+body.style.paddingTop = mainHeader.clientHeight
+body.style.paddingBottom = mainFooter.clientHeight
