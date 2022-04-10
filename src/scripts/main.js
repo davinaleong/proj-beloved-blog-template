32954f27e5b4926017826d5126d5aa317cf24c0f
Davina Leong <davina.leong.sy@gmail.com>
2022-04-10 18:11:28 +0800

    Styled header  n footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
-console.log("Script Loaded")
+const bodyOnLoad = () => {
+    window.onload(() => {
+        const toggle = document.querySelector('button[data-toggle="menu"] img')
+        const mainHeader = document.querySelector(".main-header")
+        const activeClass = 'active';
+
+        toggle.addEventListener('click', () => {
+            console.log('Toggle clicked')
+            mainHeader.classList.toggle(activeClass)
+        })
+
+        window.onresize(() => {
+            if (window.innerWidth >= 768) {
+                mainHeader.classList.remove(activeClass)
+            }
+        })
+
+        const body = document.querySelector("body")
+        const mainFooter = document.querySelector(".main-footer")
+        body.style.paddingTop = mainHeader.clientHeight
+        body.style.paddingBottom = mainFooter.clientHeight
+    })
+}
